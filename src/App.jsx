2aea81f09008c65c77e02324e42487883caecce2
Arif Hossain <arif.raj9911@gmail.com>
2024-08-01 11:43:28 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+import CustomSelects from "./components/CustomSelects"
+
+function App() {
+
+  return (
+    <div className='kzui-app_container'>
+      <CustomSelects></CustomSelects>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CustomSelects.jsx
@@ -0,0 +1,19 @@
+import './CustomSelects.css'
+
+const CustomSelects = () => {
+    return (
+        <div className='kzui-custom_container'>
+            <h1>Custom Selects Options</h1>
+            <div>
+                <select name="" id="">
+                    <option value="">Select an options</option>
+                    <option value="">Apple</option>
+                    <option value="">Banana</option>
+                    <option value="">Jack-Fruit</option>
+                </select>
+            </div>
+        </div>
+    );
+};
+
+export default CustomSelects;
