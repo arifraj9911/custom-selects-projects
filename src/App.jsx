7ff349424576dd65caecb9403be12d234bed2ba3
Arif Hossain <arif.raj9911@gmail.com>
2024-08-01 12:36:48 +0600

    select option added

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,17 @@ import './App.css'
 import CustomSelects from "./components/CustomSelects"
 
 function App() {
+  const options = [
+    { value: "Apple", label: "Apple" },
+    { value: "Orange", label: "Orange" },
+    { value: "WaterMelon", label: "WaterMelon" },
+    { value: "Banana", label: "Banana" },
+    { value: "Mango", label: "Mango" },
+  ];
 
   return (
     <div className='kzui-app_container'>
-      <CustomSelects></CustomSelects>
+      <CustomSelects options={options}></CustomSelects>
     </div>
   )
 }
--- src/components/CustomSelects.jsx
+++ src/components/CustomSelects.jsx
@@ -1,19 +1,30 @@
-import './CustomSelects.css'
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import "./CustomSelects.css";
 
-const CustomSelects = () => {
-    return (
-        <div className='kzui-custom_container'>
-            <h1>Custom Selects Options</h1>
-            <div>
-                <select name="" id="">
-                    <option value="">Select an options</option>
-                    <option value="">Apple</option>
-                    <option value="">Banana</option>
-                    <option value="">Jack-Fruit</option>
-                </select>
-            </div>
-        </div>
-    );
+const CustomSelects = ({ options }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectValue, setSelectValue] = useState("Select value");
+
+  const handleSelectValue = (e) => {
+    // console.log(e.target.innerText)
+    setSelectValue(e.target.innerText);
+  };
+
+  return (
+    <div className="kzui-custom_container">
+      <h1 className="kzui-custom_heading">Custom Selects Options</h1>
+      <div onClick={() => setIsOpen(!isOpen)} className="kzui-select_container">
+        <span className="kzui-select_title">{selectValue}</span>
+        {isOpen &&
+          options?.map((item, index) => (
+            <ul onClick={handleSelectValue} key={index}>
+              <li className="kzui-select_list_item">{item.label}</li>
+            </ul>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default CustomSelects;
